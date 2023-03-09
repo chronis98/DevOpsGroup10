21aa638dfade55970cef4490bce6a2a585af3910
Arie Bisfki <arie_bisfki@live.nl>
2023-03-09 18:06:30 +0100

    gym overview chronis

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,38 +1,34 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import GymOverview from '../views/GymOverview.vue'
 import GymDetails from '../views/GymDetails.vue'
-import { propsToAttrMap } from '@vue/shared';
+import {propsToAttrMap} from '@vue/shared';
 
-const RouteName = Object.freeze({
-  GYM_OVERVIEW: 'GymOverview'
+export const RouteName = Object.freeze({
+    GYM_OVERVIEW: 'GymOverview',
+    GYM_DETAILS: 'GymDetails'
 });
 
-
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/gym',
-      name: RouteName.GYM_OVERVIEW, // just to 
-      component: GymOverview, //loaded it the start
-      //props: {name : true}
-    },
-    {
-      path: '/',
-      // name: 'GymOverview', // just to 
-      // component: GymOverview //loaded it the start
-      redirect: {name: RouteName.GYM_OVERVIEW}
-    },
-    {
-      path: '/gym-details/:id',
-      name: GymDetails, // just to 
-      component: GymDetails,
-      
-    }
-   
-
-
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/gym',
+            name: RouteName.GYM_OVERVIEW, // just to
+            component: GymOverview, //loaded it the start
+            //props: {name : true}
+        },
+        {
+            path: '/',
+            // name: 'GymOverview', // just to
+            // component: GymOverview //loaded it the start
+            redirect: {name: RouteName.GYM_OVERVIEW}
+        },
+        {
+            path: '/gym-details/:name',
+            name: RouteName.GYM_DETAILS,
+            component: GymDetails,
+        }
+    ]
 })
 
 export default router
--- frontend/src/views/GymOverview.vue
+++ frontend/src/views/GymOverview.vue
@@ -1,89 +1,89 @@
-
 <template>
 
 
-<div class="Title">Gym Overview</div><br><br>
-    <div class="outer">
+	<div class="Title">Gym Overview</div>
+	<br><br>
+	<div class="outer">
 
-        <div v-for="l in locations">
-            <!-- <div class="card" v-on:click="handleClick(l),$emit('GymItem',l.name)"> -->
-            <div class="card" v-on:click="handleClick(l)">
-            <!-- <img src="img_avatar.png" alt="Avatar" style="width:100%"> -->
-                    <div class="container">
-                        {{ l.name }}<br>
-                        {{ l.date }}
-                    </div>
-            </div>
-        
-        </div>
-    </div>
+		<div v-for="l in locations">
+			<!-- <div class="card" v-on:click="handleClick(l),$emit('GymItem',l.name)"> -->
+			<div class="card" @click="handleClick(l.name)">
+				<!-- <img src="img_avatar.png" alt="Avatar" style="width:100%"> -->
+				<div class="container">
+					{{ l.name }}<br>
+					{{ l.date }}
+				</div>
+			</div>
 
-<div class="outer">
+		</div>
+	</div>
 
-    <div class="card">
+	<div class="outer">
 
-        <!-- <img src="img_avatar.png" alt="Avatar" style="width:100%"> -->
-        <div class="container">
-            John Doe
-            <p>Architect & Engineer</p>
-        </div>
+		<div class="card">
 
-    </div>
-</div>
+			<!-- <img src="img_avatar.png" alt="Avatar" style="width:100%"> -->
+			<div class="container">
+				John Doe
+				<p>Architect & Engineer</p>
+			</div>
+
+		</div>
+	</div>
 
 </template>
 
-  <!-- <style>
-  @media (min-width: 1024px) {
-    .about {
-      min-height: 100vh;
-      display: flex;
-      align-items: center;
-    }
-    
-  }
+<!-- <style>
+@media (min-width: 1024px) {
+	.about {
+		min-height: 100vh;
+		display: flex;
+		align-items: center;
+	}
 
-  
-  </style> -->
-
-  <!-- <script lang="ts">
-
-    </script> -->
-
-<script>
-import GymDetails from '../views/GymDetails.vue'
-export default {
-    components: {
-        GymDetails
-    },
-    name: "GymOverview",
-    data() {
-        return {
-
-            locations: [
-                { name: 'London', date: '11/02/2022', numberOfPeople: 4, complete: true },
-                { name: 'Paris', date: '12/01/2022', numberOfPeople: 2, complete: true },
-                { name: 'Tokyo', date: '04/06/2021', numberOfPeople: 6, complete: true },
-                { name: 'Mumbai', date: '08/10/2021', numberOfPeople: 10, complete: true },
-                { name: 'New York', date: '12/12/2022', numberOfPeople: 14, complete: true },
-                { name: 'Dubai', date: '10/02/2023', numberOfPeople: 12, complete: false },
-                { name: 'Shanghai', date: '04/02/2020', numberOfPeople: 2, complete: true }
-            ],
-            name: "xronis"
-        }
-    },
-    methods : {
-        handleClick(label) {
-            console.log(label.name);
-            this.$router.push({path: '/GymDetails'});
-            //<app-input :name="xronis"></app-input>
-         //this.$router.push({name: 'GymDetails', params: {  id: 'xronis'}});
-        }
-        
-    }
-    // props: {
-    //         GymName: String
-    //     }
 }
+
+
+</style> -->
+
+<!-- <script lang="ts">
+
+	</script> -->
+
+<script lang="ts">
+	import GymDetails from '../views/GymDetails.vue';
+	import {defineComponent} from "vue";
+	import type {Location} from "@/views/GymDetails.vue";
+	import {useRouter} from "vue-router";
+	import {RouteName} from "@/router";
+
+	export default defineComponent({
+		components: {
+			GymDetails
+		},
+		name: 'GymOverview',
+		setup() {
+			const locations: Location[] = [
+				{name: 'London', date: '11/02/2022', numberOfPeople: 4, complete: true},
+				{name: 'Paris', date: '12/01/2022', numberOfPeople: 2, complete: true},
+				{name: 'Tokyo', date: '04/06/2021', numberOfPeople: 6, complete: true},
+				{name: 'Mumbai', date: '08/10/2021', numberOfPeople: 10, complete: true},
+				{name: 'New York', date: '12/12/2022', numberOfPeople: 14, complete: true},
+				{name: 'Dubai', date: '10/02/2023', numberOfPeople: 12, complete: false},
+				{name: 'Shanghai', date: '04/02/2020', numberOfPeople: 2, complete: true}
+			];
+
+			const router = useRouter();
+
+			function handleClick(name: string): void {
+				router.push({name: RouteName.GYM_DETAILS, params: {name}});
+			}
+
+			return {
+				locations,
+				handleClick
+			};
+		}
+	});
 
 </script>
