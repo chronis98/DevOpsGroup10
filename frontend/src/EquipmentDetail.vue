4da0c73af94f07674a51e58fdf07a082c4aace31
AbdulZor <abdulzor@outlook.com>
2023-03-16 10:38:52 +0100

    initial layout and structure for gym detail

--- frontend/src/EquipmentDetail.vue
+++ frontend/src/EquipmentDetail.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="center">
-    <div class="equipment-detail-container" v-if="equipment">
-      <img :src="equipment.imgPath" :alt="equipment.name">
+    <div class="container" v-if="equipment">
+      <img class="image" :src="equipment.imgPath" :alt="equipment.name">
 
+      <div class="details-container">
+        <label>Name: </label> <input :value="equipment.name" disabled>
+        <span>First Added: </span> <span>{{ equipment.firstAdded.toLocaleString() }}</span>
+        <span>Reports: </span> <span>{{ equipment.reports }}</span>
+        <span>Confirmed: </span> <span>{{ equipment.confirmed.toLocaleString() }}</span>
+      </div>
+
+      <div class="button-container">
+        <button>Stars</button>
+        <button>Add Report</button>
+      </div>
     </div>
   </div>
 </template>
@@ -26,6 +37,18 @@ export default defineComponent({
       required: true
     }
   },
+  filters: {
+    formattedDate(date: Date) {
+      const options: Intl.DateTimeFormatOptions = {
+
+        // day: "numeric",
+        // month: "numeric",
+        // year: "numeric"
+      }
+      return date.toDateString();
+      // return new Intl.DateTimeFormat('default', options).format(date);
+    }
+  },
   setup(props) {
     const equipmentRef = ref<Equipment | null>(null);
     getEquipment(props.id).then(equipment => equipmentRef.value = equipment);
@@ -33,7 +56,7 @@ export default defineComponent({
     async function getEquipment(id: Number = 0): Promise<Equipment> {
       return {
         imgPath: "https://www.bestusedgymequipment.com/wp-content/uploads/2018/04/olympic-flat-bench-300x300.jpg",
-        name: "",
+        name: "Bench Press",
         firstAdded: new Date(),
         reports: 10,
         confirmed: new Date()
@@ -55,8 +78,28 @@ export default defineComponent({
   border: red solid 1px;
 }
 
-.equipment-detail-container {
+.container {
+  background-color: var(--vt-c-grey-soft);
+  box-shadow: 4px 11px 15px 3px rgba(0, 0, 0, 0.25);
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;
+}
 
+.details-container {
+  display: grid;
+  grid-templte-column: 25% 75%;
+}
+
+.button-container {
+
+}
+
+.image {
+  max-width: 20vw;
+  max-height: 20vh;
 }
 
 </style>
