f4a2435431079978956b683afa678b07829e1b4c
AbdulZor <abdulzor@outlook.com>
2023-03-16 18:04:21 +0100

    removed boilerplate code in CMS project and made initial structure of cms overview page

--- cms/src/views/HomeView.vue
+++ cms/src/views/HomeView.vue
@@ -1,9 +1,47 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div>
+    <SideBar/>
+    <main>
+    </main>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from "vue";
+import SideBar from "@/views/SideBar.vue";
+
+type User = {
+  id: number,
+  email: string,
+  type: string // TODO:: chose string because ERD has VARCHAR. Check if this has to be an enum
+}
+
+export default defineComponent({
+  name: "HomeView",
+  components: {
+    SideBar
+  },
+  setup() {
+    const usersRef = ref<User[] | null>(null);
+    getUsers().then(users => usersRef.value = users);
+
+    async function getUsers(): Promise<User[]> {
+      return [
+        {id: 1, email: "abdul@zor.nl", type: "type1"},
+        {id: 1, email: "vahip@zor.nl", type: "type1"},
+        {id: 1, email: "abva@zor.nl", type: "type2"}
+      ]
+    }
+
+    return {
+      users: usersRef
+    }
+  }
+});
+
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped lang="scss">
+
+
+</style>
---
+++ cms/src/views/SideBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="sidenav">
+    <a>LOGO</a>
+    <a>Users</a>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "SideBar"
+});
+</script>
+
+<style scoped>
+.sidenav {
+  height: 100%;
+  width: 160px;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: #111;
+  overflow-x: hidden;
+  padding-top: 20px;
+}
+
+.sidenav a {
+  padding: 6px 8px 6px 16px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #818181;
+  display: block;
+}
+
+.sidenav a:hover {
+  color: #f1f1f1;
+}
+</style>
