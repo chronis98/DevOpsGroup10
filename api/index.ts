767c83277c57c7c00e2dc956cc29d6572ba49c9b
Chronis Kontomaris <xmd@protonmail.com>
2023-03-21 15:11:23 +0100

    endpoints

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,15 @@
 import express, {Request, Response} from 'express';
 import "reflect-metadata"
-import {AppDataSource} from "./data-source";
 import dotenv from 'dotenv';
+dotenv.config();
+import {AppDataSource} from "./data-source";
+
 import cors from "cors";
 import Equipment from "./models/Equipment";
 import Report from "./models/Report";
-import Gym from "./models/Gym";
+import Gym from "./models/Gym"
 
-console.log(process.env);
+
 
 AppDataSource.initialize()
     .then(async () => {
@@ -16,7 +18,7 @@ AppDataSource.initialize()
 
       app.use(cors());
 
-      const port = process.env.PORT;
+      const port = process.env.API_PORT;
 
       app.get('/api/equipment', async (req: Request, res: Response) => {
         const equipments = await AppDataSource.manager.find(Equipment, {
---
+++ frontend/src/models/Gym.ts
@@ -0,0 +1,10 @@
+import Address from '@/models/Address'
+
+type Gym = {
+"id": number,
+"name": string,
+"imagePath":"",
+"address": Address
+}
+
+export default Gym;
